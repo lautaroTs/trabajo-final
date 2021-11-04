8537637da28e363855e0623c012706ef28487f83
Luis Becerra <luis.becerra.15.12@gmail.com>
2021-11-04 20:49:10 -0300

    configuraciones de seguridad

--- Inmobiliaria/src/main/java/com/example/demo/servicio/UsuarioService.java
+++ Inmobiliaria/src/main/java/com/example/demo/servicio/UsuarioService.java
@@ -3,6 +3,9 @@ package com.example.demo.servicio;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entidades.Usuario;
@@ -10,7 +13,7 @@ import com.example.demo.errores.ErrorServicio;
 import com.example.demo.repositorio.UsuarioRepositorio;
 
 @Service
-public class UsuarioService {
+public class UsuarioService implements UserDetailsService{
 
 	@Autowired
 	private UsuarioRepositorio usuarioRepositorio;
@@ -82,6 +85,12 @@ public class UsuarioService {
 		}else {
 			throw new ErrorServicio("No se encontro el usuario solicitado");
 		}
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
