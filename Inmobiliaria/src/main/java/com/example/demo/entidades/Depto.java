aff33296b9b25b374018b3e162563d0955e8f0b0
Luis Becerra <luis.becerra.15.12@gmail.com>
2021-11-09 00:43:21 -0300

    Sin errores y arreglado servicio administracion

--- Inmobiliaria/src/main/java/com/example/demo/entidades/Depto.java
+++ Inmobiliaria/src/main/java/com/example/demo/entidades/Depto.java
@@ -2,12 +2,7 @@ package com.example.demo.entidades;
 
 import javax.persistence.Entity;
 
-import lombok.Data;
-
-
-
 @Entity
-@Data
 public class Depto extends Propiedad {
 
 	protected Boolean amoblado;
@@ -18,55 +13,87 @@ public class Depto extends Propiedad {
 	protected Integer ambientes;
 	protected Boolean seguridad;
 	protected Boolean mascotas;
+	
+	public Depto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Depto(Boolean amoblado, Boolean espCompartidos, Integer piso, Boolean patio, Integer dormitorios,
+			Integer ambientes, Boolean seguridad, Boolean mascotas) {
+		super();
+		this.amoblado = amoblado;
+		this.espCompartidos = espCompartidos;
+		this.piso = piso;
+		this.patio = patio;
+		this.dormitorios = dormitorios;
+		this.ambientes = ambientes;
+		this.seguridad = seguridad;
+		this.mascotas = mascotas;
+	}
+
 	public Boolean getAmoblado() {
 		return amoblado;
 	}
+
 	public void setAmoblado(Boolean amoblado) {
 		this.amoblado = amoblado;
 	}
+
 	public Boolean getEspCompartidos() {
 		return espCompartidos;
 	}
+
 	public void setEspCompartidos(Boolean espCompartidos) {
 		this.espCompartidos = espCompartidos;
 	}
+
 	public Integer getPiso() {
 		return piso;
 	}
+
 	public void setPiso(Integer piso) {
 		this.piso = piso;
 	}
+
 	public Boolean getPatio() {
 		return patio;
 	}
+
 	public void setPatio(Boolean patio) {
 		this.patio = patio;
 	}
+
 	public Integer getDormitorios() {
 		return dormitorios;
 	}
+
 	public void setDormitorios(Integer dormitorios) {
-		this.dormitorios= dormitorios;
+		this.dormitorios = dormitorios;
 	}
+
 	public Integer getAmbientes() {
 		return ambientes;
 	}
+
 	public void setAmbientes(Integer ambientes) {
 		this.ambientes = ambientes;
 	}
+
 	public Boolean getSeguridad() {
 		return seguridad;
 	}
+
 	public void setSeguridad(Boolean seguridad) {
 		this.seguridad = seguridad;
 	}
+
 	public Boolean getMascotas() {
 		return mascotas;
 	}
+
 	public void setMascotas(Boolean mascotas) {
 		this.mascotas = mascotas;
 	}
-	
 
-	
 }
--- Inmobiliaria/src/main/java/com/example/demo/servicio/AdministracionService.java
+++ Inmobiliaria/src/main/java/com/example/demo/servicio/AdministracionService.java
@@ -6,11 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entidades.Administracion;
-import com.example.demo.entidades.Propietario;
 import com.example.demo.entidades.Usuario;
 import com.example.demo.errores.ErrorServicio;
 import com.example.demo.repositorio.AdministracionRepositorio;
-import com.example.demo.repositorio.PropietarioRepositorio;
 import com.example.demo.repositorio.UsuarioRepositorio;
 
 @Service
@@ -18,92 +16,123 @@ public class AdministracionService {
 
 	@Autowired
 	private AdministracionRepositorio administracionRepositorio;
-		
+
 	@Autowired
 	private UsuarioRepositorio usuarioRepositorio;
 	
 	@Autowired
-	private PropietarioRepositorio propietarioRepositorio;
-	
-	
-		public void CrearAdministracion(String direccion, String idu, String idp) throws ErrorServicio{
-			
-			Validar(direccion, idu, idp);
-			
-			Administracion administracion = new Administracion();
-			
-			Optional<Usuario> respuesta = usuarioRepositorio.findById(idu);
-			Optional<Propietario> respuesta1 = propietarioRepositorio.findById(idp);
-			
-			if(respuesta.isPresent()) {
-				
-				Usuario usuario = respuesta.get();
-				
-				administracion.setNombre(usuario.getNombre());
-				administracion.setApellido(usuario.getApellido());
-				administracion.setContrasenia(usuario.getContrasenia());
-				administracion.setEmail(usuario.getEmail());
-				administracion.setId(usuario.getId());
-				administracion.setNivelAcceso(4);
-				
-			}else {
-				throw new ErrorServicio("No se encontro la administracion solicitada");
-			}
+	private UsuarioService usuarioService;
+
+	// Crear usuario administracion teniendo ya el usuario creado
+	public Administracion CrearAdministracion(String direccion, String id) throws ErrorServicio {
+
+		Validar(direccion, id);
+
+		Administracion administracion = new Administracion();
+
+		Optional<Usuario> respuesta = usuarioRepositorio.findById(id);
+
+		if (respuesta.isPresent()) {
+
+			Usuario usuario = respuesta.get();
+
+			administracion.setNombre(usuario.getNombre());
+			administracion.setApellido(usuario.getApellido());
+			administracion.setContrasenia(usuario.getContrasenia());
+			administracion.setEmail(usuario.getEmail());
+			administracion.setId(usuario.getId());
+			administracion.setNivelAcceso(4);
 
 			administracion.setDireccion(direccion);
-			administracion.setPropietario(respuesta1.get());
+			administracionRepositorio.save(administracion);
+
+			return administracion;
+		} else {
+			throw new ErrorServicio("No se encontro el usuario solicitado.");
+		}
+
+	}
+
+	public Administracion CrearUsuarioAdministracion(String nombre, String apellido, String email, String contrasenia,
+			String direccion) throws ErrorServicio {
+		try {
+			Usuario usuario = usuarioService.CrearUsuario(nombre, apellido, email, contrasenia);
+			Administracion administracion = new Administracion();
+			
+			administracion.setNombre(usuario.getNombre());
+			administracion.setApellido(usuario.getApellido());
+			administracion.setContrasenia(usuario.getContrasenia());
+			administracion.setEmail(usuario.getEmail());
+			administracion.setId(usuario.getId());
+			administracion.setNivelAcceso(4);
+			administracion.setDireccion(direccion);
 			
 			administracionRepositorio.save(administracion);
+			
+			return administracion;			
+		} catch (Exception e) {
+			throw new ErrorServicio("No se pudo crear el usuario administracion.");
 		}
-		
-		public void Validar (String direccion, String idu, String idp) throws ErrorServicio{
-						
-			if(direccion == null || direccion.isEmpty()) {
-				throw new ErrorServicio("La direccion de la administracion no puede ser null");
-			}
-			
-			if(idu == null || idu.isEmpty()) {
-				throw new ErrorServicio("El idUsuario de la administracion no puede ser null");
-			}
-			
-			if(idp == null || idp.isEmpty()) {
-				throw new ErrorServicio("El idPropiedad de la administracion no puede ser null");
-			}
+	}
+
+	public void Validar(String direccion, String id) throws ErrorServicio {
+
+		if (direccion == null || direccion.isEmpty()) {
+			throw new ErrorServicio("La direccion de la administracion no puede ser null");
 		}
-		
-		public void ModificarAdministracion (String id, String nombre, String apellido, String contrasenia, String email, String direccion, String idp) throws ErrorServicio  {
-			
-			Optional<Administracion> respuesta = administracionRepositorio.findById(id);
-			Propietario propietario = propietarioRepositorio.getById(idp);
-			
-			if(respuesta.isPresent()) {
-				
-				Administracion administracion = respuesta.get();
-				
-				administracion.setNombre(nombre);
-				administracion.setApellido(apellido);
-				administracion.setEmail(email);
-				administracion.setContrasenia(contrasenia);
-				administracion.setDireccion(direccion);
-				administracion.setPropietario(propietario);
-				
-				administracionRepositorio.save(administracion);
-			}else {
-				throw new ErrorServicio("No se encontro la administracion solicitada");
-			}
+
+		if (id == null || id.isEmpty()) {
+			throw new ErrorServicio("El idUsuario de la administracion no puede ser null");
 		}
-		
-		public void EliminarAdministracion(String id) throws ErrorServicio{
-			Optional<Administracion> respuesta = administracionRepositorio.findById(id);
-			
-			if(respuesta.isPresent()) {
-				
-				Administracion administracion = respuesta.get();
-				
-				administracionRepositorio.delete(administracion);
-				
-			}else {
-				throw new ErrorServicio("No se encontro la administracion solicitada");
-			}
+	}
+
+	public void Validar(String direccion, String idu, String idp) throws ErrorServicio {
+
+		if (direccion == null || direccion.isEmpty()) {
+			throw new ErrorServicio("La direccion de la administracion no puede ser null");
 		}
+
+		if (idu == null || idu.isEmpty()) {
+			throw new ErrorServicio("El idUsuario de la administracion no puede ser null");
+		}
+
+		if (idp == null || idp.isEmpty()) {
+			throw new ErrorServicio("El idPropiedad de la administracion no puede ser null");
+		}
+	}
+
+	public void ModificarAdministracion(String id, String nombre, String apellido, String contrasenia, String email,
+			String direccion) throws ErrorServicio {
+
+		Optional<Administracion> respuesta = administracionRepositorio.findById(id);
+
+		if (respuesta.isPresent()) {
+
+			Administracion administracion = respuesta.get();
+
+			administracion.setNombre(nombre);
+			administracion.setApellido(apellido);
+			administracion.setEmail(email);
+			administracion.setContrasenia(contrasenia);
+			administracion.setDireccion(direccion);
+
+			administracionRepositorio.save(administracion);
+		} else {
+			throw new ErrorServicio("No se encontro el usuario administracion solicitado");
+		}
+	}
+
+	public void EliminarAdministracion(String id) throws ErrorServicio {
+		Optional<Administracion> respuesta = administracionRepositorio.findById(id);
+
+		if (respuesta.isPresent()) {
+
+			Administracion administracion = respuesta.get();
+
+			administracionRepositorio.delete(administracion);
+
+		} else {
+			throw new ErrorServicio("No se encontro la administracion solicitada");
+		}
+	}
 }
