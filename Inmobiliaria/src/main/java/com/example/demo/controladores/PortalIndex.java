219f838a25d96ff27094dadefd637dc7f671cce5
Luis Becerra <luis.becerra.15.12@gmail.com>
2021-11-23 19:26:05 -0300

    Viendo thymeleaf

--- Inmobiliaria/src/main/java/com/example/demo/controladores/PortalIndex.java
+++ Inmobiliaria/src/main/java/com/example/demo/controladores/PortalIndex.java
@@ -1,6 +1,7 @@
 package com.example.demo.controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,4 +13,10 @@ public class PortalIndex {
 	public String inicio() {
 		return "00-index";
 	}
+
+	@GetMapping("/QuienesSomos")
+	public String quienesSomos() {
+		return "01-quienesSomos.html";
+	}
+
 }
