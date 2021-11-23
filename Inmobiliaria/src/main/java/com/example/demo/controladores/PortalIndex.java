0956f55f93636ce0cb680975c4fcc55d5b0e9366
Lautaro Tsernotopulos <lautaro.tsernotopulos1110@gmail.com>
2021-11-23 20:48:33 -0300

    Index

--- Inmobiliaria/src/main/java/com/example/demo/controladores/PortalIndex.java
+++ Inmobiliaria/src/main/java/com/example/demo/controladores/PortalIndex.java
@@ -15,8 +15,55 @@ public class PortalIndex {
 	}
 
 	@GetMapping("/QuienesSomos")
-	public String quienesSomos() {
+	public String quienesSomos(Model model) {
+		
+		model.addAttribute("", model);
 		return "01-quienesSomos.html";
 	}
+	
+	@GetMapping("/RegistroUsuario")
+	public String registroUsuario() {
+		return "02-registroUsuario.html";
+	}
 
+	@GetMapping("/Login")
+	public String login() {
+		return "04-login.html";
+	}
+	
+	@GetMapping("/Propietario")
+	public String propietario() {
+		return "05-propietario.html";
+	}
+	
+	@GetMapping("/Inquilino")
+	public String inquilino() {
+		return "06-inquilino.html";
+	}
+	
+	@GetMapping("/RegistroPropiedad")
+	public String registroPropiedad() {
+		return "07-registroPropiedad.html";
+	}
+	
+	@GetMapping("/RespuestaBusqueda")
+	public String respuestaBusqueda() {
+		return "08-respuestaBusqueda.html";
+	}
+	
+	@GetMapping("/TerminosYCondiciones")
+	public String terminosYCondiciones() {
+		return "09-terminosYCondiciones.html";
+	}
+	
+	@GetMapping("/NuestrosServicios")
+	public String nuestrosServicios() {
+		return "10-nuestrosServicios.html";
+	}
+	
+	@GetMapping("/RecuperoDeContraseña")
+	public String recuperoDeContraseña() {
+		return "11-recuperoDeContraseña.html";
+	}
+	
 }
